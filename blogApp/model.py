4b437061438bfcea0103f5c58b73f0fbf73cd801
Yasir Ali <91179875+yasirali786110@users.noreply.github.com>
2024-08-16 19:43:09 +0500

    changing	in	models

--- blogApp/model.py
+++ blogApp/model.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     published_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
